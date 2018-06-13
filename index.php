f754788ce26bf97465e40d6c8dd2a63347bf3e18
lsudteenl <39257650+lsudteenl@users.noreply.github.com>
2018-06-13 11:40:59 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+$to = 'carabaocothitsupport@cbd.freshservice.com';
+$subject = 'Marriage Proposal';
+$message = 'Hi Jane, will you marry me?'; 
+$from = 'ruttanon.n@carabao.co.th';
+ 
+// Sending email
+if(mail($to, $subject, $message)){
+    echo 'Your mail has been sent successfully.';
+} else{
+    echo 'Unable to send email. Please try again.';
+}
+?>
