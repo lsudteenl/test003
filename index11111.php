c74bb68c83822a3325977a69d4017ecf69fb43cf
lsudteenl <39257650+lsudteenl@users.noreply.github.com>
2018-06-18 12:36:52 +0700

    Update index11111.php

--- index11111.php
+++ index11111.php
@@ -1,13 +1,28 @@
 <?php
-$to = 'carabaocothitsupport@cbd.freshservice.com';
-$subject = 'Marriage Proposal';
-$message = 'Hi Jane, will you marry me?'; 
-$from = 'ruttanon.n@carabao.co.th';
- 
-// Sending email
-if(mail($to, $subject, $message, $from)){
-    echo 'Your mail has been sent successfully.';
-} else{
-    echo 'Unable to send email. Please try again.';
+
+$request = new HttpRequest();
+$request->setUrl('https://notify-api.line.me/api/notify');
+$request->setMethod(HTTP_METH_POST);
+
+$request->setHeaders(array(
+  'Postman-Token' => '10a88757-7c56-48f2-915f-0330f32ac265',
+  'Cache-Control' => 'no-cache',
+  'Authorization' => 'Bearer V3HChiiaa8fNbiNZO443971MHf43LEk0U1273mAcDYI',
+  'Content-Type' => 'application/x-www-form-urlencoded',
+  'content-type' => 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW'
+));
+
+$request->setBody('------WebKitFormBoundary7MA4YWxkTrZu0gW
+Content-Disposition: form-data; name="message"
+
+Ticket Complete
+http://itsupport.carabao.co.th/helpdesk/tickets
+------WebKitFormBoundary7MA4YWxkTrZu0gW--');
+
+try {
+  $response = $request->send();
+
+  echo $response->getBody();
+} catch (HttpException $ex) {
+  echo $ex;
 }
-?>
